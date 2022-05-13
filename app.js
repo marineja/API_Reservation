5e19fccfa55102f994dadd425f6472f8821faef6
marineja <marine.jalabert@laposte.net>
2022-05-13 16:28:09 +0200

    route get train et route post reservation

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.use('/api/train', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'train',
+      title: 'Mon train',
+      description: 'Les infos de mon train',
+      heuredepart: '11h',
+      heurearrivé: '13h',
+      garededepart: 'montpellier',
+      garedarriver:'paris',
+      trainId: 'qsomihvqios'
+    },
+   
+  ];
+  res.status(200).json(stuff);
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+app.post('/api/reservation', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'réservation créé !'
+  });
 });
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
 module.exports = app;
